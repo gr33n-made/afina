ffaa3684e470b85baced91fb0d1a767501fc69c2
asimandia <you@example.com>
2018-01-25 18:00:59 +0300

    Coroutine

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -8,24 +8,23 @@ namespace Afina {
 namespace Coroutine {
 
 void Engine::Store(context &ctx) {
-    char end_stack;
+    char stack_end;
+
+    ctx.Hight = StackBottom;
+    ctx.Low = &stack_end;
 
     if (std::get<0>(ctx.Stack) != nullptr)
         delete std::get<0>(ctx.Stack);
 
-    ctx.Low = &end_stack;
-    ctx.Hight = StackBottom;
-
-    ptrdiff_t len = StackBottom - &end_stack;
-    ctx.Stack = std::make_tuple(new char[len], len);
-    memcpy(std::get<0>(ctx.Stack), &end_stack, len);
+    ctx.Stack = std::make_tuple(new char[ctx.Hight-ctx.Low], ctx.Hight-ctx.Low);
+    memcpy(std::get<0>(ctx.Stack), ctx.Low, ctx.Hight-ctx.Low);
 }
 
 void Engine::Restore(context &ctx) {
-    char end_stack;
 
-    if (ctx.Low < &end_stack)
-        Restore(ctx);
+    char stack_end;
+    if (ctx.Low < &stack_end)
+        //Restore(ctx);
 
     memcpy(StackBottom - std::get<1>(ctx.Stack), std::get<0>(ctx.Stack), std::get<1>(ctx.Stack));
     longjmp(ctx.Environment, 1);
@@ -36,16 +35,15 @@ void Engine::yield() {
         return;
 
     context *ctx_for_schedule = alive;
-    while (ctx_for_schedule == cur_routine && ctx_for_schedule != nullptr)
+    while (ctx_for_schedule == cur_routine) {
         ctx_for_schedule = ctx_for_schedule->next;
-
-    if (ctx_for_schedule == nullptr)
-        return;
+        if (ctx_for_schedule == nullptr)
+            return;
+    }
 
     if (cur_routine != nullptr) {
         Store(*cur_routine);
-
-        if (setjmp(cur_routine->Environment) != 0)
+        if (setjmp(cur_routine->Environment)!=0)
             return;
     }
 
@@ -58,13 +56,11 @@ void Engine::sched(void *routine_) {
 
     // find routine in list for execute
     ctx_for_schedule = alive;
-    while (ctx_for_schedule != ctx && ctx_for_schedule != nullptr) {
+    while (ctx_for_schedule != ctx) {
         ctx_for_schedule = ctx_for_schedule->next;
+        if (ctx_for_schedule == nullptr)
+            return;
     }
-
-    // if not found routine return
-    if (ctx_for_schedule == nullptr)
-        return;
 
     if (cur_routine != nullptr) {
         Store(*cur_routine);
@@ -72,7 +68,6 @@ void Engine::sched(void *routine_) {
         if (setjmp(cur_routine->Environment) != 0)
             return;
     }
-
     cur_routine = ctx;
     Restore(*cur_routine);
 }
